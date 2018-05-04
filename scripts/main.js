28c16ac25eed82ae3e5c214b19a14b10df208f92
Ben Powell <powell.907@osu.edu>
2018-05-04 12:28:41 -0400

    added metadata and more style updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
 var image = document.querySelector('img');
 var header = document.querySelector('h1');
 var button1 = document.querySelector('button');
-var userName='';
+var userName='.';
 
-if (!localStorage.getItem('name')){
-    setName();
-}else{
-    userName=localStorage.getItem('name');
-    header.textContent='hey '+userName;;
-}
+//if (!localStorage.getItem('name')){
+//    setName();
+//}else{
+//    userName=localStorage.getItem('name');
+//    header.textContent='hey '+userName;;
+//}
 image.onmouseover=function() {
     if (image.getAttribute('src')==='images/face.png'){
         image.setAttribute('src','images/face2.jpg');
