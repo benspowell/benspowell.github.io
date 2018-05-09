d8c91583bc9c2c4bd21db06f98d5b18a2d76545a
Ben Powell <powell.907@osu.edu>
2018-05-09 13:11:46 -0400

    logo

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,17 @@
-var image = document.querySelector('img');
-var header = document.querySelector('h1');
-var button1 = document.querySelector('button');
-var userName='.';
+var logo = document.querySelector("img[id='logo']");
 
-//if (!localStorage.getItem('name')){
-//    setName();
-//}else{
-//    userName=localStorage.getItem('name');
-//    header.textContent='hey '+userName;;
-//}
-image.onmouseover=function() {
-    if (image.getAttribute('src')==='images/face.png'){
-        image.setAttribute('src','images/face2.jpg');
-    }else{
-        image.setAttribute('src','images/face.png');
-    }
-}
-function setName(){
-    userName=prompt('Enter your name');
-    header.textContent='hey '+userName;
-    localStorage.setItem('name',userName);
-}
-button1.onclick=function(){
-    setName();
-}
-header.onclick=function() {
-    header.textContent=header.textContent+' '+userName
+logo.onmouseover= function(){
+        logo.setAttribute('src','images/logo/1.png');
+        setTimeout(function(){
+            logo.setAttribute('src','images/logo/2.png');
+        }, 500);
+        setTimeout(function(){
+            logo.setAttribute('src','images/logo/3.png');
+        }, 500);
+        setTimeout(function(){
+            logo.setAttribute('src','images/logo/4.png');
+        }, 500);
+        setTimeout(function(){
+            logo.setAttribute('src','images/logo/5.png');
+        }, 500);
 }
