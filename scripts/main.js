73a5a5c8c455fa8a34fc93834193591823dda19d
Ben Powell <powell.907@osu.edu>
2018-05-10 11:30:52 -0400

    worked on logo

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,11 @@
 var logo = document.querySelector("img[id='logo']");
 
 logo.onmouseover= function(){
-if (logo.getAttribute("src")==="images/logo/1.png"){
+    logo.setAttribute("src","images/logo-reverse.gif");
     setTimeout(function(){
-        logo.setAttribute('src','images/logo/2.png');
+        logo.setAttribute("src","images/logo-forward.gif");
         setTimeout(function(){
-            logo.setAttribute('src','images/logo/3.png');
-            setTimeout(function(){
-                logo.setAttribute('src','images/logo/4.png');
-                setTimeout(function(){
-                    logo.setAttribute('src','images/logo/5.png');
-                }, 100);
-            }, 100);
-        }, 100);
-    }, 100);
-}else{
-    setTimeout(function(){
-        logo.setAttribute('src','images/logo/4.png');
-        setTimeout(function(){
-            logo.setAttribute('src','images/logo/3.png');
-            setTimeout(function(){
-                logo.setAttribute('src','images/logo/2.png');
-                setTimeout(function(){
-                    logo.setAttribute('src','images/logo/1.png');
-                }, 100);
-            }, 100);
-        }, 100);
-    }, 100);
-}
+            logo.setAttribute("src","images/logo-still.gif");
+    }, 2250);
+    }, 2275);
 }
