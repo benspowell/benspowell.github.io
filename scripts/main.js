4dbe7c24a52c9762cf9e4a17dfe48beb948ce01d
Ben Powell <powell.907@osu.edu>
2018-05-10 10:06:43 -0400

    logo and remove filler

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,31 @@
 var logo = document.querySelector("img[id='logo']");
 
 logo.onmouseover= function(){
-        logo.setAttribute('src','images/logo/1.png');
-        setTimeout(function(){
-            logo.setAttribute('src','images/logo/2.png');
-        }, 500);
+if (logo.getAttribute("src")==="images/logo/1.png"){
+    setTimeout(function(){
+        logo.setAttribute('src','images/logo/2.png');
         setTimeout(function(){
             logo.setAttribute('src','images/logo/3.png');
-        }, 500);
+            setTimeout(function(){
+                logo.setAttribute('src','images/logo/4.png');
+                setTimeout(function(){
+                    logo.setAttribute('src','images/logo/5.png');
+                }, 100);
+            }, 100);
+        }, 100);
+    }, 100);
+}else{
+    setTimeout(function(){
+        logo.setAttribute('src','images/logo/4.png');
         setTimeout(function(){
-            logo.setAttribute('src','images/logo/4.png');
-        }, 500);
-        setTimeout(function(){
-            logo.setAttribute('src','images/logo/5.png');
-        }, 500);
+            logo.setAttribute('src','images/logo/3.png');
+            setTimeout(function(){
+                logo.setAttribute('src','images/logo/2.png');
+                setTimeout(function(){
+                    logo.setAttribute('src','images/logo/1.png');
+                }, 100);
+            }, 100);
+        }, 100);
+    }, 100);
+}
 }
