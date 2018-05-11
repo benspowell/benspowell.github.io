9fe0a49e59a94b101dd153cdbce7452455bb58ca
Ben Powell <powell.907@osu.edu>
2018-05-11 13:02:18 -0400

    logo animation and footer

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 var logo = document.querySelector("img[id='logo']");
+pickLogoColor();
+var icons = document.querySelectorAll("img[id='footer-icon']");
 
-logo.onmouseover= function(){
-    logo.setAttribute("src","images/logo-colors/"+Math.floor(Math.random()*16)+".png");
+function pickLogoColor(){
+        logo.setAttribute("src","images/logo-colors/"+Math.floor(Math.random()*16)+".png");
     setTimeout(function(){
         logo.setAttribute("src","images/logo-colors/"+Math.floor(Math.random()*16)+".png");
         setTimeout(function(){
@@ -27,3 +29,38 @@ logo.onmouseover= function(){
     }, 50);
     }, 50);
 }
+
+function othersGrey(posInIcons){
+    for (j=0; j<icons.length;j++){        
+        if (j!==posInIcons){
+                otherIcon=icons.item(j);
+                path=otherIcon.getAttribute("src");
+                path=path.substring(0,path.indexOf("-"))+"-grey.png";
+                otherIcon.setAttribute("src",path);
+            }
+    }
+}
+var path;
+var icon;
+var otherIcon;
+var i;
+var j;
+for(i = 0; i<icons.length;i++){
+    icon = icons.item(i);
+    icon.setAttribute("pos",i);
+    icon.onmouseover=function(){
+        othersGrey(1);
+    }
+    icon.onmouseout=function(){
+    for (j=0; j<icons.length;j++){        
+            otherIcon=icons.item(j);
+            path=otherIcon.getAttribute("src");
+            path=path.substring(0,path.indexOf("-"))+"-blk.png";
+            otherIcon.setAttribute("src",path);
+        }
+    }
+}
+    
+
+
+logo.onmouseover= function(){pickLogoColor();}
