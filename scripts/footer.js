8ff4dc2f592d0729c966479a77aa618b9f8bf8bc
Powell <powell.907@osu.edu>
2018-11-27 11:27:41 -0500

    footer

---
+++ scripts/footer.js
@@ -0,0 +1,37 @@
+var icons = document.querySelectorAll("img[id='footer-icon']");
+function othersGrey(posInIcons){
+    for (j=0; j<icons.length;j++){        
+        if (j!==posInIcons){
+                otherIcon=icons.item(j);
+                path=otherIcon.getAttribute("src");
+                path=path.substring(0,path.indexOf("-"))+"-grey.png";
+                otherIcon.setAttribute("src",path);
+            }
+    }
+}
+var path;
+var icon;
+var otherIcon;
+var i;
+var j;
+for(i = 0; i<icons.length;i++){
+    icon = icons.item(i);
+    icon.setAttribute("pos",i);
+    icon.onmouseout=function(){
+    for (j=0; j<icons.length;j++){        
+            otherIcon=icons.item(j);
+            path=otherIcon.getAttribute("src");
+            path=path.substring(0,path.indexOf("-"))+"-wht.png";
+            otherIcon.setAttribute("src",path);
+        }
+    }
+}
+    
+icons.item(0).onmouseover=function(){othersGrey(0);}
+icons.item(1).onmouseover=function(){othersGrey(1);}
+icons.item(2).onmouseover=function(){othersGrey(2);}
+icons.item(3).onmouseover=function(){othersGrey(3);}
+icons.item(4).onmouseover=function(){othersGrey(4);}
+icons.item(5).onmouseover=function(){othersGrey(5);}
+icons.item(6).onmouseover=function(){othersGrey(6);}
+icons.item(7).onmouseover=function(){othersGrey(7);}
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 var logo = document.querySelector("img[id='logo']");
 pickLogoColor();
-var icons = document.querySelectorAll("img[id='footer-icon']");
 var url = document.querySelector("h1[id='url']");
 function pickLogoColor(){
         logo.setAttribute("src","images/logo-colors/"+Math.floor(Math.random()*16)+".png");
@@ -30,45 +29,6 @@ function pickLogoColor(){
     }, 50);
 }
 
-//url.textContent=window.location.href;
-
-function othersGrey(posInIcons){
-    for (j=0; j<icons.length;j++){        
-        if (j!==posInIcons){
-                otherIcon=icons.item(j);
-                path=otherIcon.getAttribute("src");
-                path=path.substring(0,path.indexOf("-"))+"-grey.png";
-                otherIcon.setAttribute("src",path);
-            }
-    }
-}
-var path;
-var icon;
-var otherIcon;
-var i;
-var j;
-for(i = 0; i<icons.length;i++){
-    icon = icons.item(i);
-    icon.setAttribute("pos",i);
-    icon.onmouseout=function(){
-    for (j=0; j<icons.length;j++){        
-            otherIcon=icons.item(j);
-            path=otherIcon.getAttribute("src");
-            path=path.substring(0,path.indexOf("-"))+"-blk.png";
-            otherIcon.setAttribute("src",path);
-        }
-    }
-}
-    
-icons.item(0).onmouseover=function(){othersGrey(0);}
-icons.item(1).onmouseover=function(){othersGrey(1);}
-icons.item(2).onmouseover=function(){othersGrey(2);}
-icons.item(3).onmouseover=function(){othersGrey(3);}
-icons.item(4).onmouseover=function(){othersGrey(4);}
-icons.item(5).onmouseover=function(){othersGrey(5);}
-icons.item(6).onmouseover=function(){othersGrey(6);}
-icons.item(7).onmouseover=function(){othersGrey(7);}
-
-
 logo.onmouseover= function(){pickLogoColor();}
 logo.onclick=function(){pickLogoColor();}
+//url.textContent=window.location.href;
